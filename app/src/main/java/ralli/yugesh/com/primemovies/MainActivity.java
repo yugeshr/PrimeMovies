a14279899af4cd6157c6f8692a1457c11aeea9c8
yugeshralli <yugeshr16@gmail.com>
2018-07-04 10:27:17 +0530

    start

---
+++ app/src/main/java/ralli/yugesh/com/primemovies/MainActivity.java
@@ -0,0 +1,13 @@
+package ralli.yugesh.com.primemovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
